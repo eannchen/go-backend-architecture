b7916fad54a9bc45d4e8a8d31fde6ec4f086f6e9
eannchen <conspivit@gmail.com>
2026-04-02 12:08:45 +0800

    feat(logger): add NoopLogger implementation for Logger interface
    - Introduced NoopLogger as a no-op implementation of the Logger interface.
    - Implemented methods for Debug, Info, Warn, Error, SetLogSink, SetContextFieldsProvider, and Sync with empty bodies.
    - This addition allows for a placeholder logger that can be used in scenarios where logging is not required.

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -22,3 +22,14 @@ type Logger interface {
 	SetContextFieldsProvider(provider ContextFieldsProviderFunc)
 	Sync() error
 }
+
+// NoopLogger is a no-op implementation of Logger.
+type NoopLogger struct{}
+
+func (n NoopLogger) Debug(ctx context.Context, message string, fields ...Fields)            {}
+func (n NoopLogger) Info(ctx context.Context, message string, fields ...Fields)             {}
+func (n NoopLogger) Warn(ctx context.Context, message string, fields ...Fields)             {}
+func (n NoopLogger) Error(ctx context.Context, message string, err error, fields ...Fields) {}
+func (n NoopLogger) SetLogSink(sink LogSinkFunc)                                            {}
+func (n NoopLogger) SetContextFieldsProvider(provider ContextFieldsProviderFunc)            {}
+func (n NoopLogger) Sync() error
